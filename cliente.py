bd8eed55387dec0abec2ad0e3ceaa5e3efa747cc
tspadua <tiagospadua@gmail.com>
2022-04-13 09:27:41 -0300

    Implemented multithreading

--- cliente.py
+++ cliente.py
@@ -9,5 +9,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect(("127.0.1.1", 5002))
   print(s)
   s.sendall(bytes(message, encoding='utf-8'))
-  dados = s.recv(1024)
-  print(f"Resposta do servidor: {dados.decode()}")
+
+  while (True):
+    dados = s.recv(1024)
+    if (dados):
+      print(f"Resposta do servidor: {dados.decode()}")
--- servidor.py
+++ servidor.py
@@ -1,31 +1,37 @@
 import socket
 from this import d
 
+from _thread import *
+import threading
+ 
+def handleConnectionThread(conexao, addr):
+    while True:
+      dados = conexao.recv(1024)
+      if (dados):
+        decoded = dados.decode()
+        print(f"Mensagem recebida: {decoded}")
+
+        inputs = decoded.split("&")
+
+        response = "Unexpected range"
+        if (len(inputs) > 1):
+          code = int(inputs[0])
+          n = int(inputs[1])
+
+          if (n > 5000 and n < 15000):
+              if ((code) > 10):
+                  response = inputs[0] + "&" + inputs[1]
+      
+        conexao.sendall(bytes(response, encoding='utf-8'))
+ 
+ 
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.bind((socket.gethostname(), 5002))
   print(s)
   s.listen()
-  conexao, addr = s.accept()
-  with conexao:
+  while True:
+    conexao, addr = s.accept()
+
     print(f"Cliente conectado: {addr}")
-    while True:
-      dados = conexao.recv(1024)
-      if not dados:
-        break
-      decoded = dados.decode()
-      print(f"Mensagem recebida: {decoded}")
-
-      inputs = decoded.split("&")
-
-      response = "Unexpected range"
-      if (len(inputs) > 1):
-        code = int(inputs[0])
-        n = int(inputs[1])
-
-        if (n > 5000 and n < 15000):
-            if ((code) > 10):
-                response = inputs[0] + "&" + inputs[1]
-
-        s.connect(("127.0.1.1", 5003))
-    
-      conexao.sendall(bytes(response, encoding='utf-8'))
+    start_new_thread(handleConnectionThread, (conexao, addr))
